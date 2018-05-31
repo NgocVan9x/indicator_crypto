2afed52b604011412a92c64597a4de91fe8bf4cf
Pham Ngoc Van <van.pham@ascendcorp.com>
2018-05-31 11:17:46 +0700

    edit reque

--- Kts_Indicator_bot.py
+++ Kts_Indicator_bot.py
@@ -1,54 +1,155 @@
-import requests  
-import datetime
-class BotHandler:
-  def __init__(self, token):
-          self.token = token
-          self.api_url = "https://api.telegram.org/bot{}/".format(token)
-  def get_updates(self, offset=None, timeout=30):
-          method = 'getUpdates'
-          params = {'timeout': timeout, 'offset': offset}
-          resp = requests.get(self.api_url + method, params)
-          result_json = resp.json()['result']
-          return result_json
-  def send_message(self, chat_id, text):
-          params = {'chat_id': chat_id, 'text': text}
-          method = 'sendMessage'
-          resp = requests.post(self.api_url + method, params)
-          return resp
-  def get_last_update(self):
-          get_result = self.get_updates()
-          if len(get_result) > 0:
-                last_update = get_result[-1]
-          else:
-                last_update = get_result[len(get_result)]
-          return last_update
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+"""
+This Bot uses the Updater class to handle the bot.
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
-greet_bot = BotHandler("617289038:AAEFcSERrsI_N65ZHTWu0EapdOGWF5rs4c4")  
-greetings = ('hello', 'hi', 'greetings', 'sup')  
-now = datetime.datetime.now()
-def main():  
-    new_offset = None
-    today = now.day
-    hour = now.hour
-    while True:
-            greet_bot.get_updates(new_offset)
-            last_update = greet_bot.get_last_update()
-            last_update_id = last_update['update_id']
-            last_chat_text = last_update['message']['text']
-            last_chat_id = last_update['message']['chat']['id']
-            last_chat_name = last_update['message']['chat']['first_name']
-            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-                        greet_bot.send_message(last_chat_id, 'Good Morning  {}'.format(last_chat_name))
-                        today += 1
-            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-                        greet_bot.send_message(last_chat_id, 'Good Afternoon {}'.format(last_chat_name))
-                        today += 1
-            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-                        greet_bot.send_message(last_chat_id, 'Good Evening  {}'.format(last_chat_name))
-                        today += 1
-            new_offset = last_update_id + 1
-if __name__ == '__main__':  
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit()
+import logging
+
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+
+reply_keyboard = [['Age', 'Favourite colour'],
+                  ['Number of siblings', 'Something else...'],
+                  ['Done']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def facts_to_str(user_data):
+    facts = list()
+
+    for key, value in user_data.items():
+        facts.append('{} - {}'.format(key, value))
+
+    return "\n".join(facts).join(['\n', '\n'])
+
+
+def start(update, context):
+    update.message.reply_text(
+        "Hi! My name is Doctor Botter. I will hold a more complex conversation with you. "
+        "Why don't you tell me something about yourself?",
+        reply_markup=markup)
+
+    return CHOOSING
+
+
+def regular_choice(update, context):
+    text = update.message.text
+    context.user_data['choice'] = text
+    update.message.reply_text(
+        'Your {}? Yes, I would love to hear about that!'.format(text.lower()))
+
+    return TYPING_REPLY
+
+
+def custom_choice(update, context):
+    update.message.reply_text('Alright, please send me the category first, '
+                              'for example "Most impressive skill"')
+
+    return TYPING_CHOICE
+
+
+def received_information(update, context):
+    user_data = context.user_data
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    del user_data['choice']
+
+    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
+                              "{}"
+                              "You can tell me more, or change your opinion on something.".format(
+                                  facts_to_str(user_data)), reply_markup=markup)
+
+    return CHOOSING
+
+
+def done(update, context):
+    user_data = context.user_data
+    if 'choice' in user_data:
+        del user_data['choice']
+
+    update.message.reply_text("I learned these facts about you:"
+                              "{}"
+                              "Until next time!".format(facts_to_str(user_data)))
+
+    user_data.clear()
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("617289038:AAEFcSERrsI_N65ZHTWu0EapdOGWF5rs4c4", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            CHOOSING: [RegexHandler('^(Age|Favourite colour|Number of siblings)$',
+                                    regular_choice,
+                                    pass_user_data=True),
+                       RegexHandler('^Something else...$',
+                                    custom_choice),
+                       ],
+
+            TYPING_CHOICE: [MessageHandler(Filters.text,
+                                           regular_choice,
+                                           pass_user_data=True),
+                            ],
+
+            TYPING_REPLY: [MessageHandler(Filters.text,
+                                          received_information,
+                                          pass_user_data=True),
+                           ],
+        },
+
+        fallbacks=[RegexHandler('^Done$', done, pass_user_data=True)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
