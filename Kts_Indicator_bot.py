c6ba43efec0979b6cb18c261c62a091c1487016e
Pham Ngoc Van <van.pham@ascendcorp.com>
2018-05-31 11:27:19 +0700

    edit reque

--- Kts_Indicator_bot.py
+++ Kts_Indicator_bot.py
@@ -106,7 +106,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("617289038:AAEFcSERrsI_N65ZHTWu0EapdOGWF5rs4c4", use_context=True)
+    updater = Updater("617289038:AAEFcSERrsI_N65ZHTWu0EapdOGWF5rs4c4")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
