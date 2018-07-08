8898483a21e3cbe963763e197372770e1ebf5724
smith pham <phamngocvances@gmail.com>
2018-07-09 01:40:20 +0700

    test_v3

--- export_image.py
+++ export_image.py
@@ -8,7 +8,7 @@ from PIL import Image
 # import matplotlib
 # matplotlib.use('Agg')
 df = pd.DataFrame()
-df['Indicator'] = ['Day Ragnge','TD Sequential','RSI(14)', 'STOCH(9,6)', 'STOCHRSI(14)','MACD(12,26,9)','ADX(14)','William%R(14)','CCI(14)','ATR(14)'
+df['Indicator'] = ['Current price','TD Sequential','RSI(14)', 'STOCH(9,6)', 'STOCHRSI(14)','MACD(12,26,9)','ADX(14)','William%R(14)','CCI(14)','ATR(14)'
     ,'HIGHS/LOWS(14)','U Oscilator(7,14,28)','ROC(9)','SMA(5)','SMA(10)','SMA(20)','SMA(50)','SMA(100)','SMA(200)'
     ,'EMA(5)','EMA(10)','EMA(20)','EMA(50)','EMA(100)','EMA(200)','total buy signals:','total sell signal:','total neutural signal:'
     ]
